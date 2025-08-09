b281a564a30c2a4a77983638ffdd0b143bc61d18
srsng <srsnng@hotmail.com>
2025-08-09 23:43:19 +0800

    添加tauri剪切板插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -26,6 +26,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
