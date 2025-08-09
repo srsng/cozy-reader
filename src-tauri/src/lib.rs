7a5a576c40f424fd8371d506cda91fa821e55178
srsng <srsnng@hotmail.com>
2025-08-09 11:17:42 +0800

    添加tauri插件-sql, window state，移除没用的config，添加包lucide-svelte，调整tauri.conf

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,10 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
--- src/lib/_config/index.ts
+++
@@ -1,3 +0,0 @@
-export { type BaseSettingConfig, DefaultBaseSettingConfig } from './BaseSettingConfig';
-
-export { type AppConfig, DEFAULT_CONFIG } from './AppConfig';
