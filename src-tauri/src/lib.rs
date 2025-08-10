42411f9de001c8654bc8a553a41a8ed1202b6944
srsng <srsnng@hotmail.com>
2025-08-10 23:10:53 +0800

    添加tauri-apps/plugin-dialog依赖

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,6 +27,7 @@ pub fn run() {
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
