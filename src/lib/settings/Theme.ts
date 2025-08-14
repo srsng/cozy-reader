0fd23e553ecd44766426f09ca5885a0df327818b
srsng <srsnng@hotmail.com>
2025-08-15 00:15:15 +0800

    规划pony主题

--- src/lib/settings/Theme.ts
+++ src/lib/settings/Theme.ts
@@ -1,10 +1,20 @@
+export type AppThemeMode = 'light' | 'dark' | 'system';
+export type AppThemeType = 'standard' | 'four_colors' | 'pony';
+
+// export interface ThemeDataMap {
+// 	standard: StandardThemeData;
+// 	four_colors: FourColorsThemeData;
+// 	pony: PonyThemeData;
+// }
+
+// export type AppThemeData = Record<AppThemeType, ThemeDataMap[AppThemeType]>;
 // 每一种type都单独保留data
 export type AppThemeData = {
 	[T in AppThemeType]: T extends 'standard'
 		? StandardThemeData
 		: T extends 'four_colors'
 			? FourColorsThemeData
-			: never;
+			: PonyThemeData;
 };
 
 enum StdTDNameEnum {
@@ -16,6 +26,21 @@ enum StdTDNameEnum {
 // 从 enum 生成 union type
 export type StdTDName = `${StdTDNameEnum}`;
 export const ALL_Std_TD_NAMES = Object.values(StdTDNameEnum);
+
+enum PonyNameEnum {
+	TwilightSparkle = 'ts',
+	Fluttershy = 'fs',
+	Applejack = 'aj',
+	RainbowDash = 'rd',
+	PinkiePie = 'pp',
+	Rarity = 'rr',
+	StarlightGlimmer = 'sg',
+	SunsetShimmer = 'ss'
+}
+// 从 enum 生成 union type
+export type PonyName = `${PonyNameEnum}`;
+export const ALL_Pony_NAMES = Object.values(PonyNameEnum);
+
 // todo
 let temp: any = {};
 for (let name in ALL_Std_TD_NAMES) {
@@ -31,13 +56,15 @@ interface FourColorsThemeData {
 	hue: number;
 }
 
+interface PonyThemeData {
+	name: PonyName;
+}
+
 export const DefaultThemeData: AppThemeData = {
 	standard: { name: 'black' },
-	four_colors: { hue: 36 }
+	four_colors: { hue: 36 },
+	pony: { name: 'sg' }
 };
-
-export type AppThemeMode = 'light' | 'dark' | 'system';
-export type AppThemeType = 'standard' | 'four_colors';
 
 export const AppThemeMode2Str: Record<AppThemeMode, string> = {
 	light: '浅色|light',
@@ -46,8 +73,9 @@ export const AppThemeMode2Str: Record<AppThemeMode, string> = {
 };
 
 export const AppThemeType2Str: Record<AppThemeType, string> = {
-	standard: '标准颜色主题',
-	four_colors: '四色颜色主题(拓展版)'
+	standard: '标准主题',
+	four_colors: '四色',
+	pony: '友谊是魔法'
 };
 
 export type ThemeSettings = {
