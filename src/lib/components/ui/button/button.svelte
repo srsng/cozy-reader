26b085661a9f5c37ec7adfddba72390f3fcb4bce
srsng <srsnng@hotmail.com>
2025-08-17 10:16:59 +0800

    微调util-btn样式

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -15,7 +15,7 @@
 				secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
 				link: 'text-primary underline-offset-4 hover:underline',
-				bar: 'bg-card text-card-foreground hover:bg-accent/60'
+				bar: 'bg-card text-card-foreground hover:bg-accent-foreground/20'
 			},
 			size: {
 				default: 'h-9 px-4 py-2 has-[>svg]:px-3',
