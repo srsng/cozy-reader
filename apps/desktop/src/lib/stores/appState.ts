ecbda65538e97d0d5a6febfd6a345b6283288160
srsng <srsnng@hotmail.com>
2025-11-30 21:53:12 +0800

    fix 修复一个字符串常量错误

--- apps/desktop/src/lib/stores/appState.ts
+++ apps/desktop/src/lib/stores/appState.ts
@@ -2,7 +2,7 @@ import { writable, type Writable } from 'svelte/store';
 import { DEFAULT_APP_STATE, type AppState } from '$lib/state/app-state';
 import { InjectionToken } from '$lib/utils/context';
 
-export const APP_STATE_KEY_STR = 'user-settings';
+export const APP_STATE_KEY_STR = 'app-state';
 // app state context Key
 export const APP_STATE = new InjectionToken<Writable<AppState>>(APP_STATE_KEY_STR);
 
